1e12e8b56979a66defc9d979da2ff978078bd599
qll <1784395960@qq.com>
2020-09-03 15:30:48 +0800

    上传

--- wx/pages/usrDetail/usrDetail.js
+++ wx/pages/usrDetail/usrDetail.js
@@ -8,9 +8,51 @@ Page({
     form: {
       name: '',
       phone: ''
-    }
+    },
+    length: 0,
+    note: '',
+    pickList: ['ex1', 'ex2', 'ex3'],
+    pickValue: '',
+    time: 60,
+    showCode: false
   },
-
+  // textarea
+  note: function (e) {
+    this.setData({
+      note: e.detail.value,
+      length: e.detail.value.length
+    })
+  },
+  // picker
+  picker: function (e) {
+    this.setData({
+      pickValue: this.data.pickList[e.detail.value]
+    })
+  },
+  // switch
+  switchChange: function (e) {
+    console.log(e.detail.value)
+  },
+  // code
+  getCode: function () {
+    var that = this;
+    that.setData({
+      showCode: true
+    })
+    let times = setTimeout(function () {
+      if (that.data.time != 0) {
+        that.setData({
+          time: that.data.time - 1
+        })
+        that.getCode()
+      } else {
+        that.setData({
+          showCode: false,
+          time: 60
+        })
+      }
+    }, 1000)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -25,7 +67,7 @@ Page({
       image: image
     });
   },
- 
+
   goend: function () {
     wx.navigateTo({
       url: '../time/time',
