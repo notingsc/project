5c094325a69a78a28d8fd78df61dc2b62459e87c
qll <1784395960@qq.com>
2020-09-03 14:20:12 +0800

    细节

--- wx/pages/user/user.js
+++ wx/pages/user/user.js
@@ -8,52 +8,56 @@ Page({
     usrList: [{
       id: 1,
       txt: '我的文档1',
-      num:17,
+      num: 17,
       image: "../../static/img/599a521472424.jpg"
       // image:"https://i.loli.net/2017/08/21/599a521472424.jpg"
     }, {
       id: 2,
       txt: '我的文档2',
-      num:27,
+      num: 27,
       image: "../../static/img/599a521472424.jpg"
       // image:"https://i.loli.net/2017/08/21/599a521472424.jpg"
-    },{
+    }, {
       id: 3,
       txt: '我的文档3',
-      num:37,
+      num: 37,
       // image: "https://file.iviewui.com/dist/2054f5af8012b54926ed6b5cfd1f166c.svg"
       image: "https://i.loli.net/2017/08/21/599a521472424.jpg"
-    },],
-   
+    }, ],
+    listdata: ''
   },
-  goDetail:function(event){
-    let id=event.currentTarget.dataset.index+1;
-    let txt=event.currentTarget.dataset.txt;
+  goDetail: function (event) {
+    let id = event.currentTarget.dataset.index + 1;
+    let txt = event.currentTarget.dataset.txt;
     console.log(event.currentTarget.dataset.txt)
-    let image=event.currentTarget.dataset.image;
+    let image = event.currentTarget.dataset.image;
     wx.navigateTo({
-      url: '../usrDetail/usrDetail?id='+id+"&txt="+txt+"&image="+image,
+      url: '../usrDetail/usrDetail?id=' + id + "&txt=" + txt + "&image=" + image,
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'test.php', //仅为示例，并非真实的接口地址
-      data: {
-        x: '',
-        y: ''
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success (res) {
-        console.log(res.data)
-      }
-    })
+    // var that = this;
+    // wx.request({
+    //   url: 'http://v.juhe.cn/weather/index?cityname=%E5%8D%97%E5%AE%81&dtype=&format=&key=93a8996232179c3acbf885090246b8b3',
+    //   url:'http://v.juhe.cn/weather/index?format=2&cityname=%E8%8B%8F%E5%B7%9E&key=93a8996232179c3acbf885090246b8b3',
+    //   url: 'http://apis.juhe.cn/cook/query?key=&menu=%E8%A5%BF%E7%BA%A2%E6%9F%BF&rn=10&pn=3',
+    //   method: 'GET',
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data);
+    //     console.log(res.data.result.today),
+    //       that.setData({
+    //         listdata: res.data.result.today,
+    //       })
+    //   }
+    // })
   },
-  
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
